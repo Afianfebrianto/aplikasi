5250486ef5a32fa16bc0d8730dfc5b352a36fecb
Afianfebrianto <afianfebrianto88@gmail.com>
2024-08-14 16:51:03 +0800

    image profile done

--- app/src/main/java/com/kkn/suithmedia/MainActivity.kt
+++ app/src/main/java/com/kkn/suithmedia/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.kkn.suithmedia
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.kkn.suithmedia.ui.theme.SuithmediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +51,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuithmediaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    BgGradient()
+                    FirstScreen()
                 }
             }
         }
@@ -31,17 +63,90 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirstScreen(){
+    Column(modifier = Modifier
+        .padding(8.dp)
+    ) {
+Row(modifier = Modifier
+    .fillMaxWidth()
+    .padding(8.dp),
+    horizontalArrangement = Arrangement.Center
+    ) {
+   ProfileImage()
 }
+        
+    }
+
+}
+@Composable
+fun ProfileImage(){
+    val imageUrl = rememberSaveable {mutableStateOf("") }
+    val painter = rememberImagePainter(
+        if (imageUrl.value.isEmpty())
+        R.drawable.person_add_24dp_e8eaed_fill0_wght400_grad0_opsz24
+        else
+        imageUrl.value
+    )
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) {
+uri: Uri? ->
+        uri?.let { imageUrl.value = it.toString() }
+    }
+
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Card(shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)
+        ) {
+            Image(painter =painter ,
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable {launcher.launch("image/*") },
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(text = "Change profile picture")
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun FirstScreenPreview() {
     SuithmediaTheme {
-        Greeting("Android")
+        FirstScreen()
     }
+}
+
+@Composable
+fun input() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Name") },
+        placeholder = { Text(text = "Name") },
+    )
+}
+
+@Composable
+fun BgGradient(){
+    val gradient = Brush.linearGradient(
+        0.0f to Color.Magenta,
+        500.0f to Color.Cyan,
+        start = Offset.Zero,
+        end = Offset.Infinite
+    )
+    Box(modifier = Modifier.background(gradient))
 }
